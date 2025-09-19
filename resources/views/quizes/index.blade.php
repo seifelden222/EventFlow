9394e0c8404dc606a7e6c721e9f6a94297969490
seifelden222 <seifeldenhamdy@gmail.com>
2025-09-19 16:09:08 +0300

    add back to events in quize/index.blade.php

--- resources/views/quizes/index.blade.php
+++ resources/views/quizes/index.blade.php
@@ -2,10 +2,16 @@
 @section('content')
 @include('layouts.navigation')
 
-<div class="container py-4">
-    <h4 class="text-white mb-4"><i class="bi bi-question-circle text-info"></i> Quizzes</h4>
+<div class="container py-4 ">
+    <div class="d-flex justify-content-between align-items-center mb-4">
 
-    <div class="row row-cols-1 row-cols-md-2 row-cols-xl-3 g-4">
+        <h4 class="text-white mb-4"><i class="bi bi-question-circle text-info"></i> Quizzes</h4>
+        <a href="{{ route('events.index') }}" class="btn btn-sm btn-outline-light rounded-3">
+            <i class="bi bi-calendar-event"></i> Back to Events
+        </a>
+
+    </div>
+    <div class="row row-cols-1 row-cols-md-2 row-cols-xl-3 g-4 align-items-center">
         @forelse($quizes as $quiz)
         <div class="col">
             @include('quizes._quiz-card', ['quiz' => $quiz])
